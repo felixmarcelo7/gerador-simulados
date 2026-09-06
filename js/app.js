d2febbdb93637641c62ed070a05e54a88cda1921
Marcelo Felix <felixmarcelo.dev@gmail.com>
2026-09-06 11:12:45 -0300

    Adiciona validacao de resposta e contagem de acertos

--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@ const enunciado = document.getElementById("enunciadoQuestao");
 const contInicial = document.getElementById("spnCont");
 const contFinal = document.getElementById("spnTot");
 const alternativasContainer = document.getElementById("alternativasContainer");
+const proxBtn = document.getElementById("proxBtn");
 let indexQuestaoAtual = 0;
 let totQuestoes = 0;
-const proxBtn = document.getElementById("proxBtn");
+let totAcertos = 0;
 
 frm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -19,11 +20,27 @@ frm.addEventListener("submit", (e) => {
 });
 
 proxBtn.addEventListener("click", () => {
+  const respostaSelecionada = alternativasContainer.querySelector(
+    'input[name="resposta"]:checked',
+  );
+
+  if (!respostaSelecionada) {
+    alert("Selecione uma alternativa!");
+    return;
+  }
+
+  const indiceResposta = Number(respostaSelecionada.value);
+
+  if (indiceResposta === questoes[indexQuestaoAtual].respostaCorreta) {
+    totAcertos++;
+  }
+
   if (indexQuestaoAtual < totQuestoes - 1) {
     indexQuestaoAtual++;
     exibirQuestao();
   } else {
-    console.log("Fim do simulado");
+    console.log(`Fim do simulado: ${totAcertos} de ${totQuestoes}`);
+    proxBtn.disabled = true;
     return;
   }
 });
